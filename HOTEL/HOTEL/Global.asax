cb0abbb43790448f34f6404edd2430d54284d4f1
giovanimds <giovanimoresco@gmail.com>
2017-04-12 08:50:47 -0300

    Initial Commit

---
+++ HOTEL/HOTEL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HOTEL.MvcApplication" Language="C#" %>
---
+++ HOTEL/HOTEL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HOTEL.Startup))]
+namespace HOTEL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

3b11d37b91b150804889d7d2947ca4f8632558c5
Giovani Moresco da Silveira <giovanimoresco@gmail.com>
2017-04-18 00:56:46 -0300

    Revert "Revert "some changes in the layout, javascript file, and a new migration""
    This reverts commit fb0a4f0b2519a89b8502772c2843a38e4ebc96a5.

---
+++ Hotel2/Hotel2/Content/JavaScript.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    
+
+    $("#First").click(function () {
+        if ($("#collapseDown").css("display") === "none") {
+            $(".HotelTab").animate({
+                opacity: "toggle",
+                top: "+=50",
+                height: "toggle"
+            }, 300, function () {
+                $("#collapseDown").show()
+                $("#collapseUp").hide()
+            })
+        } else {
+            $(".HotelTab").animate({
+                opacity: "toggle",
+                top: "+=50",
+                height: "toggle"
+            }, 300, function () {
+                $("#collapseDown").hide()
+                $("#collapseUp").show()
+            })
+        }
+        
+
+    })
+
+})
